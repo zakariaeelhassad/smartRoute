9bcc56d9eaac4eea67a2a08ad58c17db51189794
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2025-10-28 11:44:32 +0100

    ajouter une exemple de application.properties

---
+++ src/test/java/org/example/smartroute/SmartRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.smartroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRouteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
