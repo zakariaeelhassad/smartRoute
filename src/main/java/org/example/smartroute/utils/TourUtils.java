f2cd1e82ba4fb22496bec1bfdff5a60ebc464236
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2025-11-01 17:32:52 +0100

    ajouter la class TourUtils dans utils

--- src/main/java/org/example/smartroute/utils/TourUtils.java
+++ src/main/java/org/example/smartroute/utils/TourUtils.java
@@ -1,4 +1,36 @@
 package org.example.smartroute.utils;
 
+import org.example.smartroute.entities.models.Delivery;
+import org.example.smartroute.entities.models.Warehouse;
+
+import java.util.List;
+
 public class TourUtils {
+    private TourUtils(){}
+
+    public static double calculateTotalDistance(Warehouse warehouse, List<Delivery> deliveries, DistanceCalculator distanceCalculator){
+        if(warehouse == null || deliveries == null || deliveries.isEmpty()) return 0;
+
+        double totalDistance = 0;
+        double currentLat = warehouse.getLatitude();
+        double currentLon = warehouse.getLongitude();
+
+        for (Delivery d : deliveries){
+            totalDistance += distanceCalculator.distance(currentLat, currentLon, d.getLatitude(), d.getLongitude());
+            currentLat = d.getLatitude();
+            currentLon = d.getLongitude();
+        }
+
+        totalDistance += distanceCalculator.distance(currentLat, currentLon, warehouse.getLatitude(), warehouse.getLongitude());
+
+        return totalDistance;
+    }
+
+    public static String formatDistance(double distanceInKm){
+        if(distanceInKm < 1){
+            return String.format("%.0f m", distanceInKm * 1000);
+        } else {
+            return String.format("%.1f Km", distanceInKm);
+        }
+    }
 }
